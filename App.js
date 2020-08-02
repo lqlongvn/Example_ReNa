b6aad5e7fc065f24cb00e477d1ad8c9678d2edab
Long <lqlongvn@gmail.com>
2020-08-02 22:14:46 +0700

    Code ban đâu với id ban đầu

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
-
 import React from 'react';
 import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar } from 'react-native';
 
 const DATA = [
   {
-    id: '1',
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
     title: 'First Item',
   },
   {
-    id: '2',
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
     title: 'Second Item',
   },
   {
-    id: '3',
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
     title: 'Third Item',
   },
 ];
